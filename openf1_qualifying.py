fd04a1017fc8cfc0eb4f580fb479163ad21bae21
itzadamh <adamheavey.dev@gmail.com>
2025-10-08 21:50:19 +0100

    New format for lap time (changed name to fastest lap)

--- openf1_qualifying.py
+++ openf1_qualifying.py
@@ -5,11 +5,24 @@ driver_url = "https://api.openf1.org/v1/drivers"
 response = requests.get(url)
 driver_response = requests.get(driver_url)
 
+# Functions
+
+def format_laptime(seconds):
+    # No lap duration, could be did not finish/start, disqualification
+    if seconds is None:
+        return "None"
+    
+    minutes = int(seconds // 60)
+    remaining_seconds = seconds % 60
+    ms = int(round((remaining_seconds - int(remaining_seconds)) * 1000, 3)) # decimal part of the seconds is multiplied by 1000
+    
+    return f"{minutes:02}:{int(remaining_seconds):02}.{ms:03}"
+
 if response.status_code & driver_response.status_code == 200:
     data = response.json()
     driver_data = driver_response.json()
     print(f"Found {len(data)} starting positions.")
-    print(f"Found {len(data)} drivers.")
+    print(f"Found {len(data)} drivers.\n")
 
     driver_number_to_names = {}
     driver_number_to_team = {}
@@ -29,15 +42,15 @@ if response.status_code & driver_response.status_code == 200:
         driver_number = item['driver_number']
         full_name = driver_number_to_names[driver_number]
         team = driver_number_to_team[driver_number]
+        laptime = format_laptime(item["lap_duration"])
 
-        print("\n")
         print(f"Driver: {full_name} | {driver_number}")
         print(f"Team: {team}")
         print(f"Position: {item['position']}")
-        print(f"Lap Duration: {item['lap_duration']}")
+        print(f"Fastest Lap: {laptime}\n")
         
         # Hide Meeting and Session key for now
-        #print(f"Meeting Key: {item['meeting_key']}")
+        # print(f"Meeting Key: {item['meeting_key']}")
         # print(f"Session Key: {item['session_key']}")
         
 else:
