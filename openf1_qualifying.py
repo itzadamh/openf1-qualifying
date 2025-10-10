20b3fb306f654d1a32fc2ef04bbe6a8d3dd0b806
itzadamh <adamheavey.dev@gmail.com>
2025-10-10 01:08:24 +0100

    Added main guard so that the script runs properly when directly executed

--- openf1_qualifying.py
+++ openf1_qualifying.py
@@ -64,50 +64,55 @@ def format_laptime(seconds):
     return f"{minutes:02}:{int(remaining_seconds):02}.{ms:03}"
 
 # ------ Main ------
-country, year = menu()
-meeting_key = get_meetingkey(country, year)
-session_key = get_sessionkey(meeting_key)
+def main():
+    country, year = menu()
+    meeting_key = get_meetingkey(country, year)
+    session_key = get_sessionkey(meeting_key)
 
-url = f"https://api.openf1.org/v1/starting_grid?session_key={session_key}&position>=1"
-driver_url = "https://api.openf1.org/v1/drivers"
+    url = f"https://api.openf1.org/v1/starting_grid?session_key={session_key}&position>=1"
+    driver_url = "https://api.openf1.org/v1/drivers"
 
-response = requests.get(url)
-driver_response = requests.get(driver_url)
+    response = requests.get(url)
+    driver_response = requests.get(driver_url)
 
-if response.status_code and driver_response.status_code == 200:
-    data = response.json()
-    driver_data = driver_response.json()
-    print(f"Found {len(data)} starting positions.")
-    print(f"Found {len(data)} drivers.\n")
+    if response.status_code and driver_response.status_code == 200:
+        data = response.json()
+        driver_data = driver_response.json()
+        print(f"Found {len(data)} starting positions.")
+        print(f"Found {len(data)} drivers.\n")
 
-    driver_number_to_names = {}
-    driver_number_to_team = {}
+        driver_number_to_names = {}
+        driver_number_to_team = {}
 
-    # Creates the dictionary with key, value pairs for drivers and their number
-    for driver in driver_data:
-        driver_number = driver['driver_number']
-        full_name = driver['full_name']
-        team = driver['team_name']
+        # Creates the dictionary with key, value pairs for drivers and their number
+        for driver in driver_data:
+            driver_number = driver['driver_number']
+            full_name = driver['full_name']
+            team = driver['team_name']
 
-        driver_number_to_names[driver_number] = full_name
-        driver_number_to_team[driver_number] = team
+            driver_number_to_names[driver_number] = full_name
+            driver_number_to_team[driver_number] = team
 
 
-    # Qualifying loop
-    for item in data:
-        driver_number = item['driver_number']
-        full_name = driver_number_to_names[driver_number]
-        team = driver_number_to_team[driver_number]
-        laptime = format_laptime(item["lap_duration"])
+        # Qualifying loop
+        for item in data:
+            driver_number = item['driver_number']
+            full_name = driver_number_to_names[driver_number]
+            team = driver_number_to_team[driver_number]
+            laptime = format_laptime(item["lap_duration"])
 
-        print(f"Driver: {full_name} | {driver_number}")
-        print(f"Team: {team}")
-        print(f"Position: {item['position']}")
-        print(f"Fastest Lap: {laptime}\n")
+            print(f"Driver: {full_name} | {driver_number}")
+            print(f"Team: {team}")
+            print(f"Position: {item['position']}")
+            print(f"Fastest Lap: {laptime}\n")
+            
+            # Hide Meeting and Session key for now
+            # print(f"Meeting Key: {item['meeting_key']}")
+            # print(f"Session Key: {item['session_key']}")
+            
+    else:
+        print("Error:", response.status_code, response.text)
         
-        # Hide Meeting and Session key for now
-        # print(f"Meeting Key: {item['meeting_key']}")
-        # print(f"Session Key: {item['session_key']}")
-        
-else:
-    print("Error:", response.status_code, response.text)
+if __name__ == "__main__":
+    main()
+    input("Press Enter to exit...")
